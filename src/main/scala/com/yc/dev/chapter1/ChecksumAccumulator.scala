5972ffeb54d36fa081413cdb878261ecb3d2ae70
姚超臣 <yaochaochen@ctv.org.cn>
2019-06-21 16:05:12 +0800

    衍生类

--- src/main/scala/com/yc/dev/chapter1/ChecksumAccumulator.scala
+++ src/main/scala/com/yc/dev/chapter1/ChecksumAccumulator.scala
@@ -25,3 +25,21 @@ class ChecksumAccumulator {
     */
 
 }
+object ChecksumAccmumlator {
+
+  private var cache = Map[String, Int]()
+  def calculate(s:String) :Int =
+    if (cache.contains(s)){
+      cache(s)
+    }else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+
+    }
+
+
+}
