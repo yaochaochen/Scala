9dfaac67879200be088d6fa9fad6be2e43d7a96a
姚超臣 <yaochaochen@ctv.org.cn>
2019-06-22 14:13:28 +0800

    case of for

---
+++ src/main/scala/com/yc/dev/chapter1/Case2_for.scala
@@ -0,0 +1,32 @@
+package com.yc.dev.chapter1
+
+object Case2_for {
+
+  def main(args: Array[String]): Unit = {
+    for (a <- 1.to(10,2)) {
+      println("Value of a:" +a)
+    }
+    println("*"*20)
+
+    for (a <- 1 to 10) {
+      println("Value of a :" +a)
+    }
+    println("*" *20)
+  }
+  for(a <- 2 until(2)) {
+    println("value of a:"+ a)
+  }
+  println("*"*20)
+  val numList = List(1,2,3,4,5,6,7,8,9,10)
+  numList.foreach(x =>{
+    println(x)
+  })
+  println("*" *20)
+  numList.filter(_>= 5).foreach(println)
+  println("*" *20)
+
+  val newList = for (a <- numList; if a != 3; if a < 8) yield a
+  newList.foreach(println)
+  val a = List(1,1,1,1,5,6,7,8,9,10)
+  a.filter(x => x == a.head).foreach(println)
+}
